0dba1a135ef29151f3a75115f135df0054c36255
Irene Nabet <irene.nabet@gmail.com>
2021-02-22 21:15:56 +0100

    opihgjvgcfx

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
-  resources :cocktails, except: :index
-  resources :cocktails, only: [:show, :create, :new] do
+  resources :cocktails, only: [:index, :show, :create, :new] do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: :destroy
